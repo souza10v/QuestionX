6406523345664bb1e0690fa5e4b99d913f588c15
Doni <donizetti.souza9@gmail.com>
2024-06-05 20:30:18 -0300

    Initial Commit

---
+++ QuestionX/QuestionXApp.swift
@@ -0,0 +1,17 @@
+//
+//  QuestionXApp.swift
+//  QuestionX
+//
+//  Created by Donizetti de Souza on 6/5/24.
+//
+
+import SwiftUI
+
+@main
+struct QuestionXApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
